914ec22af2c3ae653f648670245ffc582b48b4dc
EllieSainsbury <47758184+EllieSainsbury@users.noreply.github.com>
2019-03-11 20:01:18 +0000

    Update background.js

--- js/background.js
+++ js/background.js
@@ -11,14 +11,14 @@ $( document ).ready(function(){
 
 
   // when the button is clicked it triggers a function
-  $('h1').onbutton('click', function (backgrounds) {
+  $('h1').onButton('click', function (event) {
 
     // 1. log to the console that you have clicked the button
 
     // this selects a randon background from above and stores it as the variable 'randomBackground'
     var randomBackground = backgrounds[Math.floor(Math.random() * backgrounds.length)];
     // this creates a string that css will understand to be a background url
-    var imageUrl = "url('./images/" + randomBackground + ".jpg')"
+    var imageUrl = "url('./images/" + randomBackground + "gabriele-garanselli.jpg')"
 
 
     // 2. using jquery, change the image url of the body background to the new one saved above
